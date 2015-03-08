aaebb60e1d766514fdcc81e414448e2b5fd6dfc1
mxbeer <max-beer@web.de>
2015-03-08 22:17:15 +0100

    Add script to clenan up

---
+++ bin/bigbench_clean-up.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+  # include global config
+current_dir="$(dirname "$0")"
+source "$current_dir/global.sh"
+
+
+output "Clean up Big-Bench log directory..."
+rm -rf $log_path/*
+
+output "Clean up Big-Bench result tables..."
+$bigbench_dir/bin/bigBench cleanQueries
+
+output "Done."
